a61f5fdd6e4b189a6f288eacb6c78c22de729592
Eric <echai009@ucr.edu>
2020-01-13 22:14:30 -0800

    First layer. Generates all possible letter combinations from length 1 to given string length.

---
+++ lab1/generatePass.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+
+// Recursively call this function to generate all possible combos of passLen from
+// The array of characters from charArr
+void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLen) {
+	
+	// Base case to print
+	if (passLen == 0) {
+		cout << (prefix) << endl;
+		return;
+	}
+
+	// Recursively find all possible combos of passLen
+	for (int i = 0; i < arrLen; i++) {
+		string newPrefix;
+
+		newPrefix = prefix + charArr[i];
+
+		testPasswordRecursion(charArr, newPrefix, passLen - 1, arrLen);
+	}
+}
+
+void testPassword(char charArr[], int passLen, int arrLen) {
+	testPasswordRecursion(charArr, "", passLen, arrLen);
+}
+
+int main() {
+	char alphabet[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	
+	/* Test function call.
+
+	testPassword(alphabet, 1, 26);
+
+	 	Expected Result: All contents of array alphabet in order
+	 	Passed Test
+	 */
+
+	for (int i = 0; i < 6; i++) {
+		testPassword(alphabet, i, 26);
+	}
+}
