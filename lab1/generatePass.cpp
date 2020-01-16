5fbfc2a130bec2135df50c9b391610990a2688d9
eric-Is-Me <chaingisme@gmail.com>
2020-01-16 13:53:53 -0800

    Finished intermediate step

--- lab1/generatePass.cpp
+++ lab1/generatePass.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
 #include <array>
 #include <string>
+//#include <bitset>
 #include <hashlib2plus/trunk/src/hashlibpp.h>
 using namespace std;
 
 // Recursively call this function to generate all possible combos of passLen from
 // The array of characters from charArr
 void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLen) {
-	
 	// Base case to print
 	if (passLen == 0) {
 		try {
-			// TODO: Replace salt with the actual salt from etc/shadow file
 			string alternateSum = md5wrap->getHashFromString(prefix + salt + prefix);
 			/* TODO: Compute intermediate0
-			hash(prefix + magic(???) + salt + length(prefix) byte of alternateSum + append NULL byte if bit is set and append first byte of prefix is unset)
+			
 			*/
+			string bytesAlt = "";
+			string appendBytes = "";
+			//string binary = bitset<4>(passLen).to_string();
+
+			for (int i = 0; i < (alternateSum.length()/passLen); ++i) {
+				bytesAlt = bytesAlt + alternateSum;
+			}
+
+			for (int i = 0; i < (alternateSum.length()%passLen); ++i) {
+				bytesAlt = bytesAlt + alternateSum[i];
+			}
+
+			for (;passLen != 0; passLen >> 1) {
+				if (passLen & 1) {
+					appendBytes = appendBytes + 0;
+				}
+				else {
+					appendBytes = appendBytes + prefix[0];
+				}
+			}
+
+			string intermediate = md5wrap->getHashFromString(prefix + "1" + salt + bytesAlt + appendBytes);
 
 			for (int i = 0; i <= 999; i++) {
 				string tmp = "";
@@ -48,6 +69,14 @@ void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLe
 		catch(h1Exception &e) {
 			cout << "Hashing error" << endl;
 		}
+
+		// TODO: Get the final hash
+
+		if (finalHash == '$1$4fTgjp6q$pgy8b7CmJFffcOr9Q.HOA1') {
+			cout << prefix << endl;
+			break;
+		}
+
 		// cout << prefix << endl;
 		return;
 	}
@@ -69,14 +98,7 @@ void testPassword(char charArr[], int passLen, int arrLen) {
 int main() {
 	char alphabet[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	hashwrapper *md5wrap = new md5wrapper();
-
-	/* Test function call.
-
-	testPassword(alphabet, 1, 26);
-
-	 	Expected Result: All contents of array alphabet in order
-	 	Passed Test
-	 */
+	const string salt = "4fTgjp6q";
 
 	for (int i = 1; i <= 6; i++) {
 		testPassword(alphabet, i, 26);
