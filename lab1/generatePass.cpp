4578aab90538a65bb7dee93c91385289c57d0bf8
ahung005 <ahung005@ucr.edu>
2020-01-18 12:23:40 -0800

    works with testSample

--- lab1/generatePass.cpp
+++ lab1/generatePass.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <bitset>
 #include "hashlib2plus/trunk/src/hashlibpp.h"
+#include <vector>
 using namespace std;
 
 // Recursively call this function to generate all possible combos of passLen from
@@ -12,6 +13,18 @@ using namespace std;
 //const string salt = "4fTgjp6q";
 const string crypt = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const string salt = "hfT7jp2q";
+
+std::vector<char> HexToBytes(const std::string& hex) {
+  std::vector<char> bytes;
+
+  for (unsigned int i = 0; i < hex.length(); i += 2) {
+    std::string byteString = hex.substr(i, 2);
+    char byte = (char) strtol(byteString.c_str(), NULL, 16);
+    bytes.push_back(byte);
+  }
+
+  return bytes;
+}
 
 // Convert direct output to human readable format
 string checkBytes(string input) {
@@ -37,6 +50,7 @@ void reverseStr(string& str) {
 void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLen) {
 	// Base case to print
     string intermediate1000;
+    std::vector<char> inter1000;
     hashwrapper *md5wrap = new md5wrapper();
 	//if (passLen == 1) {
         prefix = "zhgnnd";
@@ -46,10 +60,17 @@ void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLe
 			string appendBytes = "";
 
             cout << alternateSum << endl;
+            cout << alternateSum.length() << endl;
+            std::vector<char> altSum = HexToBytes(alternateSum);
+            for (int i = 0; i < altSum.size(); i++) {
+                cout << altSum.at(i) << " ";
+            }
+            cout << endl;
 
             for (int i = 0; i < prefix.length(); i++) {
-                bytesAlt = bytesAlt + alternateSum[i];
-            };
+                //bytesAlt = bytesAlt + alternateSum[i];
+                bytesAlt = bytesAlt + altSum[i];
+            }
             cout << "bytes: " <<  bytesAlt << endl;
 
             /*
@@ -107,13 +128,21 @@ void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLe
             
 
 			string intermediate = checkBytes(md5wrap->getHashFromString(prefix + "$1$" + salt + bytesAlt + appendBytes));
-            cout << intermediate << endl;
+            std::vector<char> inter = HexToBytes(intermediate);
+            if(intermediate == "ed7a5307588e49ed3a2777d926d62f96") {
+                cout << intermediate  + " pass" << endl;
+            } else {
+                cout << intermediate + " fail" << endl;
+            }
 
 			for (int i = 0; i <= 999; i++) {
 				string tmp = "";
 
 				if (i % 2 == 0) {
-					tmp += intermediate;
+					//tmp += intermediate;
+                    for(int j = 0; j < inter.size(); j++) {
+                        tmp += inter.at(j);
+                    }
 				}
 				else {
 					tmp += prefix;
@@ -131,13 +160,23 @@ void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLe
 					tmp += prefix;
 				}
 				else {
-					tmp += intermediate;
+					//tmp += intermediate;
+                    for(int j = 0; j < inter.size(); j++) {
+                        tmp += inter.at(j);
+                    }
 				};
 
 				intermediate = checkBytes(md5wrap->getHashFromString(tmp));
+                inter = HexToBytes(intermediate);
 			}
             intermediate1000 = intermediate;
-            cout << intermediate1000 << endl;
+            inter1000 = HexToBytes(intermediate1000);
+            if(intermediate1000 == "ff202f2e9b6ac6e495570536fc89fd2a") {
+                cout << intermediate1000 + " pass" << endl;
+            }
+            else {
+                cout << intermediate1000 + " fail" << endl;
+            }
 		}
 		catch(hlException &e) {
 			cout << "Hashing error" << endl;
@@ -145,22 +184,38 @@ void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLe
 
 		// TODO: Get the final hash
         string finalSum = "";
-        finalSum += intermediate1000.at(11);
-        finalSum += intermediate1000.at(4);
-        finalSum += intermediate1000.at(10);
-        finalSum += intermediate1000.at(5);
-        finalSum += intermediate1000.at(3);
-        finalSum += intermediate1000.at(9);
-        finalSum += intermediate1000.at(15);
-        finalSum += intermediate1000.at(2);
-        finalSum += intermediate1000.at(8);
-        finalSum += intermediate1000.at(14);
-        finalSum += intermediate1000.at(1);
-        finalSum += intermediate1000.at(7);
-        finalSum += intermediate1000.at(13);
-        finalSum += intermediate1000.at(0);
-        finalSum += intermediate1000.at(6);
-        finalSum += intermediate1000.at(12);
+        //finalSum += intermediate1000.at(11);
+        //finalSum += intermediate1000.at(4);
+        //finalSum += intermediate1000.at(10);
+        //finalSum += intermediate1000.at(5);
+        //finalSum += intermediate1000.at(3);
+        //finalSum += intermediate1000.at(9);
+        //finalSum += intermediate1000.at(15);
+        //finalSum += intermediate1000.at(2);
+        //finalSum += intermediate1000.at(8);
+        //finalSum += intermediate1000.at(14);
+        //finalSum += intermediate1000.at(1);
+        //finalSum += intermediate1000.at(7);
+        //finalSum += intermediate1000.at(13);
+        //finalSum += intermediate1000.at(0);
+        //finalSum += intermediate1000.at(6);
+        //finalSum += intermediate1000.at(12);
+        finalSum += inter1000.at(11);
+        finalSum += inter1000.at(4);
+        finalSum += inter1000.at(10);
+        finalSum += inter1000.at(5);
+        finalSum += inter1000.at(3);
+        finalSum += inter1000.at(9);
+        finalSum += inter1000.at(15);
+        finalSum += inter1000.at(2);
+        finalSum += inter1000.at(8);
+        finalSum += inter1000.at(14);
+        finalSum += inter1000.at(1);
+        finalSum += inter1000.at(7);
+        finalSum += inter1000.at(13);
+        finalSum += inter1000.at(0);
+        finalSum += inter1000.at(6);
+        finalSum += inter1000.at(12);
         //11 4 10 5 3 9 15 2 8 14 1 7 13 0 6 12
         
         string b128 = "";
@@ -181,7 +236,7 @@ void testPasswordRecursion(char charArr[], string prefix, int passLen, int arrLe
         }
         reverseStr(final); 
         string finalHash = "$1$" + salt + "$" + final;
-
+        cout << finalHash << endl;
 		//if (finalHash == "$1$4fTgjp6q$pgy8b7CmJFffcOr9Q.HOA1") {
         if (finalHash == "$1$hfT7jp2q$wPwz7GC6xLt9eQZ9eJkaq.") {
 			cout << prefix  + " success " <<endl;
